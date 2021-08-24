20a088bde9ed3a8bb6156d3ef61b69ec8b7a6d62
GUO0430 <77811646+GUO0430@users.noreply.github.com>
2021-08-24 12:06:52 +1200

    employerForm added (#4)

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Navigate, useRoutes } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+import FormLayout from './layouts/FormLayout';
 //
 import Login from './pages/Login';
 import Register from './pages/Register';
 import DashboardApp from './pages/DashboardApp';
 import Products from './pages/Products';
 import Blog from './pages/Blog';
+import Form from './pages/EmployerForm';
 import User from './pages/User';
 import NotFound from './pages/Page404';
 
@@ -32,9 +34,17 @@ export default function Router() {
       children: [
         { path: 'login', element: <Login /> },
         { path: 'register', element: <Register /> },
+        { path: 'form', element: <Form /> },
         { path: '404', element: <NotFound /> },
         { path: '/', element: <Navigate to="/app" /> },
         { path: '*', element: <Navigate to="/404" /> }
+      ]
+    },
+    {
+      path: '/form',
+      element: <FormLayout />,
+      children: [
+        { path: '/', element: <Form /> },
       ]
     },
 
