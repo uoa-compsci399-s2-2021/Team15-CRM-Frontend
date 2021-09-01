0d4d125c6eb2c479c3da825764950839484b55ec
Qiuheihei <lukaiqiu0626@gmail.com>
2021-09-01 17:33:35 +1200

    Make the dialog of each product

--- src/components/form/FirstPage.js
+++ src/components/form/FirstPage.js
@@ -43,7 +43,6 @@ export class FirstPage extends Component {
     }
   }
 
-
   render() {
     const { values, handleChange, handleStartChecked, handleCloseChecked } = this.props;
     const isError = (condition) => this.state.showErrors && condition;
